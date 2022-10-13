12c172f4bdf7cd0f000406e9247d564064ea3da1
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2022-10-13 22:00:35 +0200

    ny burger

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
 import "./style.css";
+
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  console.log("hej123");
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+// document.querySelector(".icon").addEventListener("click", myFunction);
+
+// function myFunction() {
+//   console.log("hej");
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
 document.getElementById("dageDropdown").addEventListener("change", filterTable);
 
 function filterTable() {
